a59a74e0b770dc8d141de366bc573682170d02cd
lixiang30 <534551900@qq.com>
2018-10-26 22:39:40 +0800

    add xadmin gloable settings

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,5 +1,23 @@
 import xadmin
 from .models import EmailVerifyRecord,Banner
+from xadmin import views
+
+# 创建xadmin的最基本管理器配置，并与view绑定
+class BaseSetting(object):
+    # 开启主题功能
+    enable_themes = True
+    use_bootswatch = True
+
+
+# 全局修改，固定写法
+class GlobalSettings(object):
+    # 修改title
+    site_title = '在线教育后台管理系统'
+    # 修改footer
+    site_footer = '在线教育平台'
+    # 收起菜单
+    menu_style = 'accordion'
+
 
 class EmailVerifyRecordAdmin(object):
     # 显示的字段
@@ -16,3 +34,8 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
 xadmin.site.register(Banner,BannerAdmin)
+
+# 将基本配置管理与view绑定
+xadmin.site.register(views.BaseAdminView,BaseSetting)
+# 将title和footer信息进行注册
+xadmin.site.register(views.CommAdminView,GlobalSettings)
