f1b05ba10598ddbd20b1d70702f6b962ee9ac2c9
lixiang30 <534551900@qq.com>
2018-10-27 23:27:30 +0800

    add user_login function

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import authenticate,login
+
+
+def user_login(request):
+    if request.method == "POST":
+        # 获取用户提交的用户名和密码
+        user_name = request.POST.get('username', None)
+        pass_word = request.POST.get('password', None)
+        # 成功返回user对象,失败None
+        user = authenticate(username=user_name, password=pass_word)
+        # 如果不是null说明验证成功
+        if user is not None:
+            # 登录
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, 'login.html', {'msg': '用户名或密码错误'})
+    elif request.method == "GET":
+        return render(request,"login.html",{})
