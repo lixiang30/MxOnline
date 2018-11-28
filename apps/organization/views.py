aefa2982c35a27d6e7f99eda7c8f8079cc2cd1aa
lixiang30 <534551900@qq.com>
2018-11-28 13:55:22 +0800

    机构分页

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import View
 from .models import CourseOrg,CityDict
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger # 分页功能需要导入的类
 
 class OrgView(View):
     """
@@ -12,8 +13,18 @@ class OrgView(View):
         all_orgs = CourseOrg.objects.all()
         org_nums = all_orgs.count()
         all_citys = CityDict.objects.all()
+        #　对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        p = Paginator(all_orgs,2,request=request)
+
+        orgs = p.page(page)
+
+
         return render(request,"org-list.html",{
-            "all_orgs":all_orgs,
+            "all_orgs":orgs,
             "all_citys":all_citys,
             "org_nums":org_nums,
         })
