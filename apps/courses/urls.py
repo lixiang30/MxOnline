a0a093a32c3bdcd484bff16b8fe2ddb2cdff722f
lixiang30 <534551900@qq.com>
2018-12-13 11:56:16 +0800

    Course list

---
+++ apps/courses/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url,include
+
+from .views import CourseListView
+
+urlpatterns = [
+    url(r'^list/$',CourseListView.as_view(),name='course_list'),
+
+
+]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+from .models import Course
+
+
+class CourseListView(View):
+    def get(self,request):
+        all_courses = Course.objects.all()
+        return render(request,"course-list.html",{
+            "all_courses":all_courses,
+        })
