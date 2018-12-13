69838b6208106f945536c2f231bfaedab2990d51
lixiang30 <534551900@qq.com>
2018-12-13 12:07:57 +0800

    课程列表分页功能

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import View
+
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger # 分页功能需要导入的类
+
 from .models import Course
 
 
 class CourseListView(View):
     def get(self,request):
         all_courses = Course.objects.all()
+
+        # 　对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        p = Paginator(all_courses, 3, request=request)
+
+        courses = p.page(page)
+
         return render(request,"course-list.html",{
-            "all_courses":all_courses,
+            "all_courses":courses,
         })
