338b31930b8df1f27cf005541dec42fb6a5fb55b
lixiang30 <534551900@qq.com>
2018-12-13 13:27:12 +0800

    课程排序、热门课程推荐

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -10,7 +10,17 @@ from .models import Course
 
 class CourseListView(View):
     def get(self,request):
-        all_courses = Course.objects.all()
+        all_courses = Course.objects.all().order_by("-add_time")
+
+        hot_courses = Course.objects.all().order_by("-click_nums")[:3]
+
+        # 课程排序
+        sort = request.GET.get('sort', '')
+        if sort:
+            if sort == "students":
+                all_courses = all_courses.order_by("-students")
+            elif sort == "hot":
+                all_courses = all_courses.order_by("-click_nums")
 
         # 　对课程机构进行分页
         try:
@@ -23,4 +33,6 @@ class CourseListView(View):
 
         return render(request,"course-list.html",{
             "all_courses":courses,
+            'sort':sort,
+            'hot_courses':hot_courses,
         })
