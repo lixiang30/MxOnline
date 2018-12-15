623eb0435ec87ff1bc44bbf4cc4f440cf9970a73
lixiang30 <534551900@qq.com>
2018-12-15 21:21:35 +0800

    ajax提交和post请求,完成用户评论功能

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 from django.views.generic.base import View
@@ -6,7 +6,7 @@ from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger # 分页功能需要导入的类
 
 from .models import Course,CourseResource
-from operation.models import UserFavorite
+from operation.models import UserFavorite,CourseComments
 
 
 class CourseListView(View):
@@ -86,6 +86,55 @@ class CourseInfoView(View):
             "course":course,
             "course_resource":all_resources,
         })
+
+
+class CommentsView(View):
+    def get(self,request,course_id):
+        course = Course.objects.get(id=int(course_id))
+        all_resources = CourseResource.objects.filter(course=course)
+        all_comments = CourseComments.objects.all()
+        return render(request,"course-comment.html",{
+            "course":course,
+            "course_resources":all_resources,
+            "all_comments":all_comments,
+        })
+
+class CommentsView(View):
+    '''课程评论'''
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        all_resources = CourseResource.objects.filter(course=course)
+        all_comments = CourseComments.objects.all()
+        return render(request, "course-comment.html", {
+            "course": course,
+            "course_resources": all_resources,
+            'all_comments':all_comments,
+        })
+
+
+#添加评论
+class AddCommentsView(View):
+    '''用户评论'''
+    def post(self, request):
+        if not request.user.is_authenticated:
+            # 未登录时返回json提示未登录，跳转到登录页面是在ajax中做的
+            return HttpResponse('{"status":"fail", "msg":"用户未登录"}', content_type='application/json')
+        course_id = request.POST.get("course_id", 0)
+        comments = request.POST.get("comments", "")
+        if int(course_id) > 0 and comments:
+            # 实例化一个course_comments对象
+            course_comments = CourseComments()
+            # 获取评论的是哪门课程
+            course = Course.objects.get(id = int(course_id))
+            # 分别把评论的课程、评论的内容和评论的用户保存到数据库
+            course_comments.course = course
+            course_comments.comments = comments
+            course_comments.user = request.user
+            course_comments.save()
+            return HttpResponse('{"status":"success", "msg":"评论成功"}', content_type='application/json')
+        else:
+            return HttpResponse('{"status":"fail", "msg":"评论失败"}', content_type='application/json')
+
 
 
 
